185a4071193ad168e379f431f3bff398ff9482e1
Mafry Rivera <mafryfernanda@gmail.com>
2020-02-22 14:23:42 -0800

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mafry Turkey Bacon Arepasburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sofi Leche Condensada Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lina Aguardiente Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Eddie The Ranch Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali Caliente Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hormigas Culonas Leche Burger', true);
